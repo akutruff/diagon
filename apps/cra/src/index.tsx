bbfe5e2fce32e6d1c6ad5a152ade1e17c140201e
akutruff <874049+akutruff@users.noreply.github.com>
2022-08-10 15:05:21 +0000

    set license and use latest version of bootstrapping react demo app

--- apps/cra/src/index.tsx
+++ apps/cra/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const container = document.querySelector('#root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>);
